e399c35d791bfc1006fd7534cd1795960ae1b69d
Carlos Acevedo <cacevedod@devco.com.co>
2020-04-28 15:22:18 -0500

    single run true para ci

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
